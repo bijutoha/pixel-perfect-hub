ab4aa73db6ee410a5db682d9d4b84be8df1547c0
bkuddin <mdbkuddin@gmail.com>
2023-11-26 12:20:10 +0600

    Footer Created & Navbar Added from DaisyUI

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import NavBar from "../Pages/Shared/Navbar/NavBar";
 
 const MainLayout = () => {
   return (
     <div>
+        <NavBar></NavBar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
