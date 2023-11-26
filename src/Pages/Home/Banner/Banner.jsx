004797c6f75dc0078041fda280733faf438d738e
bkuddin <mdbkuddin@gmail.com>
2023-11-26 18:50:30 +0600

    Designed Other Slides in Carousel

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -12,7 +12,9 @@ const Banner = () => {
       <div>
         <div className="hero min-h-screen bg-[#2D88FF] text-start px-10">
           <div className="flex items-center justify-between flex-col lg:flex-row-reverse">
-            <div><img src={img1} className="max-w-xl mt-20" /></div>
+            <div>
+              <img src={img1} className="max-w-xl mt-20" />
+            </div>
             <div className="md:w-3/5">
               <h3 className="text-2xl uppercase text-white font-bold">
                 Mastering Shadows
@@ -23,8 +25,7 @@ const Banner = () => {
               </h1>
               <p className="py-6">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
+                assumenda excepturi exercitationem quasi.
               </p>
               <button className="btn bg-[#FFD700] px-10 rounded-xl text-xl text-[#2D88FF] mb-16">
                 Get Quote
@@ -33,21 +34,24 @@ const Banner = () => {
           </div>
         </div>
       </div>
+      {/* 2nd Slide */}
       <div>
-        <div className="hero min-h-screen bg-[#330066] text-start">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src={img2} className="max-w-xl " />
+        <div className="hero min-h-screen bg-[#330066] text-start px-10">
+          <div className="flex items-center justify-between flex-col lg:flex-row-reverse">
             <div>
+              <img src={img2} className="max-w-2xl" />
+            </div>
+            <div className="md:w-3/5 mt-10">
               <h3 className="text-2xl uppercase text-white font-bold">
                 Precision in Pixels
               </h3>
               <h1 className="text-5xl font-bold text-[#FFD700] ">
-                Professional Clipping Path Services
+                Professional <br /> Clipping Path Services
               </h1>
-              <p className="py-6 text-white">
+              <p className="py-6 md:pr-36 text-white">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
+                assumenda excepturi exercitationem quasi. <br /> In deleniti
+                eaque aut repudiandae et a id nisi.
               </p>
               <button className="btn bg-[#FFD700] px-10 rounded-xl text-xl text-[#330066]">
                 Get Quote
@@ -56,21 +60,22 @@ const Banner = () => {
           </div>
         </div>
       </div>
+      {/* Slide 3 */}
       <div>
-        <div className="hero min-h-screen bg-[#2D88FF] text-start">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src={img3} className="max-w-xl " />
-            <div>
+        <div className="hero min-h-screen bg-[#2D88FF] text-start px-10">
+          <div className="flex justify-between items-center flex-col lg:flex-row-reverse">
+            <img src={img3} className="max-w-2xl " />
+            <div className="md:w-3/5 mt-10">
               <h3 className="text-2xl uppercase text-white font-bold">
                 ChromaChic Studio
               </h3>
               <h1 className="text-5xl font-bold text-[#FFD700] ">
-                Transforming Colors, Defining Paths
+                Transforming Colors, <br /> Defining Paths
               </h1>
-              <p className="py-6">
+              <p className="py-6 text-white md:pr-36">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
+                assumenda excepturi exercitationem quasi. <br /> In deleniti
+                eaque aut repudiandae et a id nisi.
               </p>
               <button className="btn bg-[#FFD700] px-10 rounded-xl text-xl text-[#2D88FF]">
                 Get Quote
@@ -79,21 +84,21 @@ const Banner = () => {
           </div>
         </div>
       </div>
+      {/* Slide 4 */}
       <div>
-        <div className="hero min-h-screen bg-[#330066] text-start">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src={img4} className="max-w-xl " />
-            <div>
+        <div className="hero min-h-screen bg-[#330066] text-start px-10">
+          <div className="flex justify-between items-center flex-col lg:flex-row-reverse">
+            <img src={img4} className="max-w-2xl mt-20 " />
+            <div className="md:w-3/5 mt-10">
               <h3 className="text-2xl uppercase text-white font-bold">
                 Invisible Artistry
               </h3>
               <h1 className="text-5xl font-bold text-[#FFD700] ">
-                Seamless Image Masking and Ghost Mannequin Edits
+                Seamless Image Masking <br /> and Ghost Mannequin Edits
               </h1>
-              <p className="py-6 text-white">
+              <p className="py-6 text-white md:pr-36">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
+                assumenda excepturi exercitationem quasi. 
               </p>
               <button className="btn bg-[#FFD700] px-10 rounded-xl text-xl text-[#330066]">
                 Get Quote
