e45493f006e3a2bd5492f3384e1b739df3305e5b
bkuddin <mdbkuddin@gmail.com>
2023-11-28 23:47:30 +0600

    Daynamic Sharable cover designed

--- src/Pages/ServicePage/ServicePageSamples/ServicePageSamples.jsx
+++ src/Pages/ServicePage/ServicePageSamples/ServicePageSamples.jsx
@@ -1,10 +1,12 @@
+import Cover from "../../Shared/Cover/Cover";
 import ShowServicePageSamples from "../ShowServicePageSamples/ShowServicePageSamples";
 
-const ServicePageSamples = ({samples}) => {
+const ServicePageSamples = ({samples, title, description, img}) => {
   
 
   return (
     <div className="px-40">
+        {title && <Cover img={img} title={title} description={description} ></Cover>}
       <div className="grid md:grid-cols-2 gap-4 my-20">
         
             {samples.map((sample) => (
--- src/Pages/Shared/Cover/Cover.jsx
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -1,6 +1,6 @@
 import { Parallax } from 'react-parallax';
 
-const Cover = ({ img }) => {
+const Cover = ({ img, title, description }) => {
   return (
     
     <Parallax
@@ -16,9 +16,9 @@ const Cover = ({ img }) => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-xl">
-          <h1 className="mb-5 text-5xl font-bold">At Pixel Perfect Hub, consider us your virtual photo magic makers!</h1>
+          <h1 className="mb-5 text-5xl font-bold">{title}</h1>
           <p className="mb-5">
-          Simply upload your images, and voila! In just a few hours, they're returned to you—pixel-perfect and ready to dazzle. Your desired size, your preferred format—no need to break a sweat. We've got your photo editing worries covered!
+           {description}
           </p>
           <button className="btn btn-primary">Get Quote</button>
         </div>
